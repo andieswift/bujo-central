c7f052811bf081aa74e5261e26b5f0e86cd9ef8b
Andie Swift <andie.swift5@gmail.com>
2020-01-13 16:41:53 -0800

    made the display of products responsive

--- client/components/productListItem.jsx
+++ client/components/productListItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ProductListItem(props) {
   const price = (props.price / 100).toFixed(2);
   return (
-    <div className='card width-30 bg-white m-2'>
+    <div className='card col-sm-12 col-md-3 col-xl-2 bg-white m-2'>
       <img className = 'img-card' src={props.image}></img>
       <div className= 'p-3'>
         <h5>{props.name}</h5>
