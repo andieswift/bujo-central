aae2cdcb2cb8c638b32769692192bd5c3ddf1821
Andie Swift <andie.swift5@gmail.com>
2020-01-15 11:44:10 -0800

    added a feature so when the user clicks on the shopping cart icon they can view the items in their cart.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -7,12 +7,17 @@ function Header(props) {
   } else {
     item = `${props.cartItemCount} Items`;
   }
+
+  function handleCartClick() {
+    props.setView('cart', {});
+  }
+
   return (
     <div className= 'p-3 col-12 bg-dark'>
       <div className='ml-5'>
         <i className='text-light fas fa-dollar-sign'></i>
         <h5 className='ml-2 d-inline text-light'>Wicked Sales</h5>
-        <i className='text-light mt-1 mr-5 fas fa-shopping-cart float-right cursor-pointer'></i>
+        <i className='text-light mt-1 mr-5 fas fa-shopping-cart float-right cursor-pointer' onClick={handleCartClick}></i>
         <p className='mr-2 d-inline text-light float-right'>{item}</p>
 
       </div>
