4c63d7a88f095d65c20b1c769636db4cc101f6f1
Andie Swift <andie.swift5@gmail.com>
2020-01-15 16:54:05 -0800

    added a checkout feature where user can click in the cart to checkout and that brings them to a checckout screen where they can enter their name, credit card number, and address and then complete order which brings them back to the main screen

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -2,17 +2,39 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
 function CartSummary(props) {
-  const cartItems = props.cartItems.map(item => {
-    return <CartSummaryItem key={item.productId} product={item} />;
-  });
+
+  let cartItems;
+  if (props.cartItems.length === 0) {
+    cartItems = <h2> Your Cart is Empty </h2>;
+  } else {
+    cartItems = props.cartItems.map(item => {
+      return <CartSummaryItem key={item.cartItemId} product={item} />;
+    });
+  }
   function handleBackClick() {
     props.setView('catalog', {});
   }
+
+  function getTotal() {
+    const priceArr = props.cartItems.map(value => value.price);
+    const total = priceArr.reduce((accumulator, currentValue) => accumulator + currentValue);
+    return (total / 100).toFixed(2);
+  }
+
+  function handleCheckoutClick() {
+    if (props.cartItems.length !== 0) {
+      props.setView('checkout', {});
+    }
+
+  }
+
   return (
     <div>
       <p className=" m-1 text-muted cursor-pointer" onClick={handleBackClick}><i className="fas fa-arrow-left"></i> Back to Catalog</p>
       <h1 className= "ml-3"> My Cart </h1>
       {cartItems}
+      <h2 className="ml-3 d-inline">Total: ${getTotal()}</h2>
+      <button type="button" className="btn btn-primary float-right mr-2" onClick={handleCheckoutClick}>Checkout</button>
     </div>
   );
 }
---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      creditCard: '',
+      shippingAddress: ''
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleCreditCardChange = this.handleCreditCardChange.bind(this);
+    this.handleShippingAddressChange = this.handleShippingAddressChange.bind(this);
+    this.handleOrder = this.handleOrder.bind(this);
+    this.handleBackClick = this.handleBackClick.bind(this);
+  }
+
+  getTotal() {
+    const priceArr = this.props.cartItems.map(value => value.price);
+    const total = priceArr.reduce((accumulator, currentValue) => accumulator + currentValue);
+    return (total / 100).toFixed(2);
+  }
+
+  handleBackClick() {
+    this.props.setView('catalog', {});
+  }
+
+  handleNameChange() {
+    this.setState({ name: event.target.value });
+  }
+
+  handleCreditCardChange() {
+    this.setState({ creditCard: event.target.value });
+  }
+
+  handleShippingAddressChange() {
+    this.setState({ shippingAddress: event.target.value });
+  }
+
+  handleOrder() {
+    if (this.state.name !== '' && this.state.creditCard !== '' && this.state.shippingAddress !== '') {
+      this.props.placeOrder({
+        name: this.state.name,
+        creditCard: this.state.creditCard,
+        shippingAddress: this.state.shippingAddress
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>My Cart</h2>
+        <p className="text-muted">Order Total: ${this.getTotal()}</p>
+        <form>
+          <div className="form-group">
+            <label>Name</label>
+            <input type="text" value = {this.state.name} onChange={this.handleNameChange} className="form-control"></input>
+          </div>
+          <div className="form-group">
+            <label>CreditCard</label>
+            <input type="text" value={this.state.CreditCard} onChange={this.handleCreditCardChange}className="form-control"></input>
+          </div>
+          <div className="form-group">
+            <label>Shipping Address</label>
+            <textarea value={this.state.shippingAddress} onChange={this.handleShippingAddressChange} className="form-control" rows="5"></textarea>
+          </div>
+        </form>
+        <p className=" m-1 text-muted cursor-pointer d-inline" onClick={this.handleBackClick}>
+          <i className="fas fa-arrow-left"></i> Continue Shopping</p>
+        <button type="button" className="btn btn-primary float-right mr-2" onClick={this.handleOrder}>Place Order</button>
+      </div>
+    );
+  }
+
+}
